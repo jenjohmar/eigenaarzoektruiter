264a17b71eec176faecacceb2309ead2b10a5fd8
jenjohmar <69906853+jenjohmar@users.noreply.github.com>
2020-12-17 18:08:04 +0100

    ezr 1

---
+++ js/ezr.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+$(".sliderslideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('.slider > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('.slider');
+},  3000);
