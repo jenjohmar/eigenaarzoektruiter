3cdbbb8605e1a9c9384b459c44851a7820ac0125
pvisser <33752862+pvisser@users.noreply.github.com>
2021-01-04 11:40:09 +0100

    Update testform_submit.php

--- testform_submit.php
+++ testform_submit.php
@@ -1,5 +1,12 @@
 <?php
-if($_POST["message"]) {
-    mail("pepijn.visser@it4mation.nl", "Form to email message", $_POST["message"], "From: an@email.address");
+if($_POST["message"]) {    
+    $success = mail("pepijn.visser@it4mation.nl", "Form to email message", $_POST["message"], "From: info@eigenaarzoekruiter.nl");
+    if (!$success) {
+        $errorMessage = error_get_last()['message'];
+        <?=$errorMessage ?>
+    } else { 
+        $thankYou = "Het is gelukt!";
+        <?=$thankYou ?>
+    }
 }
 ?>
