67793419f780ab1f0af12c3e67bf8e088a9e99a0
jenjohmar <69906853+jenjohmar@users.noreply.github.com>
2021-01-30 22:15:47 +0100

    commit 1 van 30-01

--- js/ezr.js
+++ js/ezr.js
@@ -6,6 +6,14 @@ document.querySelector('.close-1').addEventListener('click', function() {
 	document.querySelector('.spotlight-1').style.display = 'none';
 });
 
+document.getElementById("leesmeer-3").addEventListener('click', function(){
+	document.querySelector(".spotlight-3").style.display = 'flex';
+});
+
+document.querySelector('.close-9').addEventListener('click', function() {
+	document.querySelector('.spotlight-3').style.display = 'none';
+});
+
 document.getElementById("leesmeer-2").addEventListener('click', function(){
 	document.querySelector(".spotlight-2").style.display = 'flex';
 });
